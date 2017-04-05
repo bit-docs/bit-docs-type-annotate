ab24e4afcd65d414f273c936781fb607c34fd921
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-05 10:48:02 -0500

    prelim docs

--- annotate.js
+++ annotate.js
@@ -1,6 +1,16 @@
 var typer = require("./lib/typer");
 var namer = require("./lib/namer");
 
+/**
+ * @module {function} bit-docs-type-annotate
+ * @parent plugins
+ *
+ * @description Utilities for parsing closure type annotations.
+ *
+ * @body
+ *
+ * TBD
+ */
 module.exports = {
     typeNameDescription: require("./lib/typeNameDescription"),
     type: function(){
